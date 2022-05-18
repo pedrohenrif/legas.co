704e40e269ab11777b1df78f8d54a7b5265bb042
Pedro Henrique <pedro.henrique.furtado.santos.@gmail.com>
2022-05-18 20:53:26 -0300

    inicio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    autoplay: true,
+    adaptiveHeigth: true,  /* se adapta a ao tamanho das imagens */
+
+});
+
+
